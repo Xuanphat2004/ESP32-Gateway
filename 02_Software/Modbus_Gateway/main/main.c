4b58e5dc13953e5452959fbcf4e3e8c11f8094c2
Xuanphat2004 <phatnguyen251104@gmail.com>
2026-03-22 00:34:11 +0700

    update menuconfig and add new function 'rtc_a_init' into file main.c

--- 02_Software/Modbus_Gateway/main/main.c
+++ 02_Software/Modbus_Gateway/main/main.c
@@ -11,28 +11,31 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_event.h"
-// Thư viện tự khởi tạo
+
+// User components
 #include "wifi.h"
 #include "ethernet.h"
 #include "modbus_rtu.h"
-#include "eeprom.h"
 #include "i2c_config.h"
+#include "eeprom.h"
+#include "rtc_mb.h"
 
 static const char *TAG = "[APP MAIN]";
 
 void app_main(void)
 {
+
     // Khởi tạo Netif chung cho tất cả các giao tiếp với mạng
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    i2c_config();
-    eeprom_init();
-
     wifi_Init();
     eth_init();
+    ESP_ERROR_CHECK(i2c_config());
 
-    // modbus_rtu_port_1_init();
+    eeprom_init();
+    esp_err_t err = rtc_a_init();
+
+    //  modbus_rtu_port_1_init();
     modbus_rtu_port_2_init();
 
     xTaskCreate((void *)modbus_test_read, "modbus_rtu_test_task", 4096, NULL, 5, NULL);
